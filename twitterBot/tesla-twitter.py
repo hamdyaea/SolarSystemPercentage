943bcba0cf70741cd0eae0a7fd27b7ef83afe14d
hamdyaea <hamdy.aea@protonmail.com>
2019-12-19 17:24:19 +0100

    tesla hashtag

--- twitterBot/tesla-twitter.py
+++ twitterBot/tesla-twitter.py
@@ -1954,7 +1954,7 @@ Tesla()
 
 
 
-message = Percentage.TeslaHTML + Percentage. barrTeslaHTML +str('\n#Astronomy #Space #Espace #Astrometry')
+message = Percentage.TeslaHTML + Percentage. barrTeslaHTML +str('\n#Astronomy #Space #Espace #Astrometry #Tesla')
 photo = open('/var/www/html/pictures/tesla.jpg', 'rb')
 response = twitter.upload_media(media=photo)
 twitter.update_status(status=message, media_ids=[response['media_id']])
