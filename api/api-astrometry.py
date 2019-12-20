1d12e6cef532592cd14873926612edac57c6637e
hamdyaea <hamdy.aea@protonmail.com>
2019-12-20 17:38:46 +0100

    public port flask

--- api/api-astrometry.py
+++ api/api-astrometry.py
@@ -24,4 +24,5 @@ class Peri(Resource):
 
 api.add_resource(Peri, "/perihelion", "/perihelion/", "/perihelion/<int:id>")
 if __name__ == '__main__':
-    app.run(debug=True)
+    #app.run(debug=True)
+    app.run(host='0.0.0.0')
