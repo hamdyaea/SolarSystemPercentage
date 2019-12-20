a3c98441c9b0812acd5814f474129ec47dd83b4f
hamdyaea <hamdy.aea@protonmail.com>
2019-12-20 17:05:22 +0100

    Add api

---
+++ api/api-astrometry.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# Developer : Hamdy Abou El Anein
+# hamdy.aea@protonmail.com
+# This is the Python api of astrometry.ch
+
+
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+import random
+from data import *
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class Peri(Resource):
+    def get(self, id=0):
+        if id == 0:
+            return random.choice(perihelion), 200
+        for peri in perihelion:
+            if (peri["id"] == id):
+                return peri, 200
+        return "Object not found", 404
+
+api.add_resource(Peri, "/perihelion", "/perihelion/", "/perihelion/<int:id>")
+if __name__ == '__main__':
+    app.run(debug=True)
