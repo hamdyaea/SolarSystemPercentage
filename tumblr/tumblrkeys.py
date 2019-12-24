7a892d0a559ce6b24af9328120e6f6429d3b59f3
hamdyaea <hamdy.aea@protonmail.com>
2019-12-24 14:56:47 +0100

    tumblr bot

---
+++ tumblr/tumblrkeys.py
@@ -0,0 +1,7 @@
+import pytumblr
+client = pytumblr.TumblrRestClient(
+    'irsyGmYuiRb8uW271v5WCnyR72jR8d27Uw9cyJJJeDT6ggjcz3',
+    'x5DM9OSQf9Pw5nEFjt6Y0fNiM0CIH26e9Ts7Yg1168WYaoOSqK',
+    'EqOUn6Rg5fQzsyuKL7N6od06YCpauSQfbJb5KverZdEWEpyvnD',
+    '8E7buUIN9oQ6KkunF04ujKVLf0tyuoshxL7h9y3tJAcaxr9Ada',
+)
