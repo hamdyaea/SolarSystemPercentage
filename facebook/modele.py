c7be7f7b574aeb68f1bfa3ad32e3dac24e076571
hamdyaea <hamdy.aea@protonmail.com>
2020-03-03 18:39:33 +0100

    Code beauty :octocat:

--- facebook/modele.py
+++ facebook/modele.py
@@ -4,7 +4,8 @@ from keys import *
 import facebook
 
 
-
 graph = facebook.GraphAPI(access_token=[token], version="3.0")
 
-graph.put_photo(image=open("/var/www/html/pictures/lune.jpg", 'rb'), message='Look at the moon !')
+graph.put_photo(
+    image=open("/var/www/html/pictures/lune.jpg", "rb"), message="Look at the moon !"
+)
