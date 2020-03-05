50955630d9a6e84c33ed7c4facc47709753da140
hamdyaea <hamdy.aea@protonmail.com>
2020-03-05 08:21:25 +0100

    Code beauty :octocat:

--- twitterBot/hale-bopp-twitter.py
+++ twitterBot/hale-bopp-twitter.py
@@ -123,9 +123,10 @@ def perihelion(object, years1, years):
                 Percentage.barrobject = "Percent of this year : " + (barre) + str("\n")
                 Percentage.barrobjectHTML = "Percent of this year : " + (barre) + ("\n")
 
+
 perihelion("Hale-Bopp", 729117, 866718)
 
 message = str(Percentage.objectHTML) + str("\n#Astronomy #Space #Espace #Astrometry")
-photo = open('/var/www/html/pictures/hale-bopp.jpg', 'rb')
+photo = open("/var/www/html/pictures/hale-bopp.jpg", "rb")
 response = twitter.upload_media(media=photo)
-twitter.update_status(status=message, media_ids=[response['media_id']])
+twitter.update_status(status=message, media_ids=[response["media_id"]])
