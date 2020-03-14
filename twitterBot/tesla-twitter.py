7e1866e32c1a21674cf12f46210ce7dbc71d83b1
hamdyaea <hamdy.aea@protonmail.com>
2020-03-14 14:44:51 +0100

    Code beauty :octocat:

--- twitterBot/tesla-twitter.py
+++ twitterBot/tesla-twitter.py
@@ -122,9 +122,10 @@ def perihelion(object, years1, years):
                 Percentage.barrobject = "Percent of this year : " + (barre) + str("\n")
                 Percentage.barrobjectHTML = "Percent of this year : " + (barre) + ("\n")
 
+
 perihelion("Tesla", 559, 559)
 
 message = str(Percentage.objectHTML) + str("\n#Astronomy #Space #Espace #Astrometry")
-photo = open('/var/www/html/pictures/tesla.jpg', 'rb')
+photo = open("/var/www/html/pictures/tesla.jpg", "rb")
 response = twitter.upload_media(media=photo)
-twitter.update_status(status=message, media_ids=[response['media_id']])
+twitter.update_status(status=message, media_ids=[response["media_id"]])
