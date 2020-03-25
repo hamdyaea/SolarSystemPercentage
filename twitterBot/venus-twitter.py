7f51468daceb1f2e117285a13b0e7411e459b5cc
hamdyaea <hamdy.aea@protonmail.com>
2020-03-25 14:12:39 +0100

    Modifiy venus nmbr of days

--- twitterBot/venus-twitter.py
+++ twitterBot/venus-twitter.py
@@ -124,7 +124,7 @@ def perihelion(object, years1, years):
                 Percentage.barrobjectHTML = "Percent of this year : " + (barre) + ("\n")
 
 
-perihelion("Venus", 215, 215)
+perihelion("Venus", 225, 225)
 
 message = str(Percentage.objectHTML) + str("\n#Astronomy #Space #Espace #Astrometry")
 photo = open("/var/www/html/pictures/venus.jpg", "rb")
