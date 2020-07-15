e1cbac2f8b1ded57af5290247e63970d284730e9
hamdyaea <hamdy.aea@protonmail.com>
2020-07-15 09:09:28 +0200

    Apophis twitter

---
+++ twitterBot/apophis-twitter.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python3
+
+# Developer : Hamdy Abou El Anein
+# hamdy.aea@protonmail.com
+
+# percentage of the year for the earth, the moon, jupiter, Saturn, etc...
+# Lunar Perigee and Apogee Calculator : https://www.fourmilab.ch/earthview/pacalc.html
+# Mercury and others : https://in-the-sky.org/newscalyear.php
+
+from datetime import date, datetime, timedelta
+import re
+import json
+
+from keys import *
+
+from twython import Twython
+
+twitter = Twython(consumer_key, consumer_secret, access_token, access_token_secret)
+
+
+class Percentage:
+    def __init__(self):
+        self.current_year
+        self.today
+        self.thisMonth
+        self.EarthResult
+        self.earthHTML
+        self.BarrEarth
+        self.BarrEarthHTML
+        self.NewobjectPerihelion
+        self.objectPerihelion
+        self.objectResult
+        self.objectHTML
+        self.barrobject
+        self.barrobjectHTML
+
+
+Percentage.current_year = date.today().year
+Percentage.today = int(datetime.today().strftime("%d"))
+Percentage.thisMonth = int(datetime.today().strftime("%m"))
+
+
+def perihelion(object, years1, years):
+    with open("/var/www/html/orbital.json", "r") as O:
+        orbit = json.load(O)
+        thisYear = orbit[object][0]["Peri"]
+        picture = orbit[object][0]["Picture"]
+        W = orbit[object][0]["PicW"]
+        H = orbit[object][0]["PicH"]
+        for i in thisYear:
+            d0Year = i[:4]
+            d0Year = int(d0Year)
+            d0Month = i[5:7]
+            d0Month = int(d0Month)
+            d0Day = i[8:10]
+            d0Day = int(d0Day)
+            d0 = date(d0Year, d0Month, d0Day)
+            d1 = date(Percentage.current_year, Percentage.thisMonth, Percentage.today)
+            # d1 = d1 + timedelta(days=1)
+            if d0 >= d1 - timedelta(days=years1) and d0 <= d1:
+                Percentage.objectPerihelion = d0
+        for i in thisYear:
+            d0Year = i[:4]
+            d0Year = int(d0Year)
+            d0Month = i[5:7]
+            d0Month = int(d0Month)
+            d0Day = i[8:10]
+            d0Day = int(d0Day)
+            d0 = date(d0Year, d0Month, d0Day)
+            d1 = date(Percentage.current_year, Percentage.thisMonth, Percentage.today)
+            if d0 <= d1 + timedelta(days=years) and d0 > d1:
+                Percentage.NewobjectPerihelion = d0
+                d1 = date(
+                    Percentage.current_year, Percentage.thisMonth, Percentage.today
+                )
+                d3 = Percentage.NewobjectPerihelion - Percentage.objectPerihelion
+                d3 = str(d3)
+                d3 = d3.split()
+                d3 = int(d3[0])
+                d3 = d3 + 1
+                ValuePercent = d3 / 100
+                delta = d1 - Percentage.objectPerihelion
+                delta = str(delta)
+                delta = delta.split()
+                delta = delta[0]
+                new = re.sub("[^0-9]", "", str(delta))
+                new = str(new)
+                new = new[:6]  #:4
+                new = int(new)
+                new = new + 1
+                Percentage.objectResult = new / ValuePercent
+                Percentage.objectResult = round(Percentage.objectResult, 2)
+
+                print(str(orbit[object][0]["info"]) + str(" : ") + str(object))
+                print(("Day of the year : ") + str("Day ") + str(new))
+                print(("Year progress : ") + str(Percentage.objectResult) + str("%"))
+
+                percent = Percentage.objectResult
+                barre = (
+                    "["
+                    + "#" * int((50 / 100) * percent)
+                    + "_" * int((50 / 100) * (100 - percent))
+                    + "]"
+                )
+                print("Percent of this year : " + (barre))
+                print("\n")
+
+                Percentage.objectHTML = (
+                    (str(orbit[object][0]["info"]) + str(" : ") + str(object))
+                    + ("\n")
+                    + str(("Day of the year : ") + str("Day ") + str(new))
+                    + ("\n")
+                    + str(
+                        ("Year progress : ")
+                        + str(Percentage.objectResult)
+                        + str("%")
+                        + ("\n")
+                        + str(barre)
+                    )
+                )
+
+                Percentage.barrobject = "Percent of this year : " + (barre) + str("\n")
+                Percentage.barrobjectHTML = "Percent of this year : " + (barre) + ("\n")
+
+
+perihelion("99942 Apophis", 324, 324)
+
+message = str(Percentage.objectHTML) + str("\n#Astronomy #Space #Espace #Astrometry")
+photo = open("/var/www/html/pictures/apophis.jpg", "rb")
+response = twitter.upload_media(media=photo)
+twitter.update_status(status=message, media_ids=[response["media_id"]])
